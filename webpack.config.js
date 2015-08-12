b8cf8b5933030deb5ac3a868931027a4a35c1803
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-08-12 11:26:26 +0100

    Apply styles to React component and make them visible in browser
    Please note we're using a plural form of the word "loader" as we now require
    multiple Webpack loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, loader: 'babel', exclude: /(node_modules)/ },
-      { test: /\.css$/, loader: 'css', exclude: /(node_modules)/ }
+      { test: /\.css$/, loaders: ['style', 'css'], exclude: /(node_modules)/ }
     ]
   }
 }
