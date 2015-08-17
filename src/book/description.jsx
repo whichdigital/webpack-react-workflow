820101caf7b7905fa7fb73edbb1a3aeacf84d49f
Rob Parsons <rob.parsons@which.co.uk>
2015-08-17 10:32:16 +0100

    Add new component to render additional scss files

---
+++ src/book/description.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './description.scss';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <p className={'green'}>This is a great book!</p>
+    )
+  }
+}
