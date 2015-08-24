d01866c0349dfc3d24affd40254828e9f0cfc7c7
Rob Parsons <rob.parsons@which.co.uk>
2015-08-24 11:32:16 +0100

    Reinstate console log and test

---
+++ spec/label_spec.js
@@ -0,0 +1,6 @@
+import 'core-js/es5';
+import Label from '../src/book/label.js'
+
+describe('Label', function() {
+  it('is true', () => expect(true).to.equal(true) );
+});
--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import Book from './book/book.jsx';
+import Label from './book/label.js'
+
+var label = new Label();
+label.logSomething();
 
 React.render( <Book />, document.querySelector( 'body' ) );
