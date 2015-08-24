0fb166314a8f4907889adeff184b53408656e9cd
Rob Parsons <rob.parsons@which.co.uk>
2015-08-24 11:32:48 +0100

    Update book spec

--- spec/book_spec.js
+++ spec/book_spec.js
@@ -1,6 +1,19 @@
 import 'core-js/es5';
-import Book from '../src/book/book.js'
+import React from 'react/addons';
+import Book from '../src/book/book.jsx'
+
+var ReactTestUtils = React.addons.TestUtils;
 
 describe('Book', function() {
-  it('is true', () => expect(true).to.equal(true) );
+  xit('is true', function() {
+    let renderer = ReactTestUtils.createRenderer();
+    renderer.render( Book );
+    let output = renderer.getRenderOutput();
+
+    expect( output.type ).to.equal( 'div' );
+  });
+
+  it('is true', function() {
+    expect( true ).to.equal( true );
+  });
 });
