b8480da67bcdb6073613ebbd3b498beeabc6e53a
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-08-24 15:55:11 +0100

    Add unit tests for the Book React class
    The intention of the tests is to make sure the `render` method returns a certain
    wrapper DOM node and it includes a couple of other React components.
    
    Still to be done: mocking these two child components.

--- spec/book_spec.js
+++ spec/book_spec.js
@@ -1,19 +1,52 @@
 import 'core-js/es5';
 import React from 'react/addons';
 import Book from '../src/book/book.jsx'
-
-var ReactTestUtils = React.addons.TestUtils;
+import Heading from '../src/book/heading.jsx'
+import Description from '../src/book/description.jsx'
 
 describe('Book', function() {
-  xit('is true', function() {
-    let renderer = ReactTestUtils.createRenderer();
-    renderer.render( Book );
-    let output = renderer.getRenderOutput();
+  let bookComponent;
 
-    expect( output.type ).to.equal( 'div' );
+  before( function() {
+    let ReactTestUtils = React.addons.TestUtils,
+      shallowRenderer = ReactTestUtils.createRenderer();
+
+    shallowRenderer.render( <Book/> );
+    bookComponent = shallowRenderer.getRenderOutput();
   });
 
-  it('is true', function() {
-    expect( true ).to.equal( true );
+  it('is wrapped in div element', function() {
+    expect( bookComponent.type ).to.equal( 'div' );
+  });
+
+  describe('checks if children exist regardless of their order', function() {
+    let children;
+
+    before( function() {
+      children = bookComponent.props.children;
+    });
+
+    it('includes Heading component', function() {
+      let childHeading = children.filter(
+        component => component.type === Heading
+      );
+      expect( childHeading.length ).to.be.ok;
+    });
+
+    it('includes Description component', function() {
+      let childDescription = children.filter(
+        component => component.type === Description
+      );
+      expect( childDescription.length ).to.be.ok;
+    });
+  });
+
+  // An alternative to the two tests above
+  it('includes Heading and Description components in particular order',
+      function() {
+    expect( bookComponent.props.children ).to.eql( [
+      <Heading />,
+      <Description />
+    ] );
   });
 });
