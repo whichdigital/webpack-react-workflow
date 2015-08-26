2395359d7336c3ca1bdbb45eeed9b2c8f670d1e6
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-08-26 14:02:27 +0100

    Add component intended to become a child of another component

---
+++ spec/description_spec.js
@@ -0,0 +1,52 @@
+import 'core-js/es5';
+import React from 'react/addons';
+import Description from '../src/book/description.jsx';
+
+var ReactTestUtils = React.addons.TestUtils;
+
+describe('Description', function() {
+  it('is a React element', function() {
+    let validation = ReactTestUtils.isElement(
+      <Description />
+    );
+
+    expect(validation).to.be.ok;
+  });
+
+  it('is a Description React element', function() {
+    let validation = ReactTestUtils.isElementOfType(
+      <Description />,
+      Description
+    );
+
+    expect(validation).to.be.ok;
+  });
+
+  it('is not a DOM component', function() {
+    let component = ReactTestUtils.renderIntoDocument(<Description />);
+    let validation = ReactTestUtils.isDOMComponent(component);
+
+    expect(validation).to.not.be.ok;
+  });
+
+  it('is a composite component', function() {
+    let component = ReactTestUtils.renderIntoDocument(<Description />);
+    let validation = ReactTestUtils.isCompositeComponent(component);
+
+    expect(validation).to.be.ok;
+  });
+
+  it('is a composite Description component', function() {
+    let component = ReactTestUtils.renderIntoDocument(<Description />);
+    let validation = ReactTestUtils.isCompositeComponentWithType(component, Description);
+
+    expect(validation).to.be.ok;
+  });
+
+  it('renders a <p> tag', function() {
+    let component = ReactTestUtils.renderIntoDocument(<Description />);
+    let descriptionElement = React.findDOMNode( component );
+
+    expect( descriptionElement.tagName ).to.equal( 'P' );
+  });
+});
