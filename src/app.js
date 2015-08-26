435092fc7502005b36a9e497a864f3f6056cae64
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-08-26 15:33:25 +0100

    Introduce composite Section component
    Demonstrate alternative ways of unit testing child components in React.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import './navigation/links.scss';
 import React from 'react';
 import Book from './book/book.js';
 
-import Heading from './book/heading.jsx'; // eslint-disable-line no-unused-vars
-React.render( <Heading />, document.getElementById( 'heading-container' ) );
+import Section from './book/section.jsx'; // eslint-disable-line no-unused-vars
+React.render( <Section />, document.getElementById( 'heading-container' ) );
 
 var book = new Book();
 book.logSomething();
